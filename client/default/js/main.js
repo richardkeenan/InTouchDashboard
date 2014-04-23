62df263e6355240706c3db749293822529c384d4
Máté Rácz <mate.racz@feedhenry.com>
2014-04-23 12:31:18 +0100

    added Login controller

--- client/default/js/main.js
+++ client/default/js/main.js
@@ -1,1 +1,11 @@
 console.log('hello');
+
+
+var app = angular.module('inTouchDashboardApp', []);
+
+app.controller('LoginController', function($scope) {
+
+    $scope.login = function() {
+        console.log($scope.username, $scope.password);
+    }
+});
